56f2adeb27aa5accfa473d74f582c9d87d617940
Balram-05 <charaniya.balram.2005@gmail.com>
2025-07-28 22:40:27 +0530

    Prepared app for deployment

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
@@ -13,7 +13,7 @@ export const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      showAlert('succes', 'Logged in succesfully');
+      showAlert('success', 'Logged in succesfully');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
@@ -27,7 +27,7 @@ export const logout = async () => {
   try {
     const res = await axios ({
         method: 'GET',
-        url: 'http://127.0.0.1:3000/api/v1/users/logout',
+        url: '/api/v1/users/logout',
     });
     if(res.data.status === 'success') location.reload(true);
   } catch (err) {
