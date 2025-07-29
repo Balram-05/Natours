443a1a1ef1146820244eb013fe51c3ec19b713a2
Balram-05 <charaniya.balram.2005@gmail.com>
2025-07-29 19:12:04 +0530

    Improved Stripe implementation 4

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,7 +53,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id; 
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items.data[0].price_data.unit_amount / 100; // Stripe stores in cents
+  const price = session.line_items.data[0].price_data.unit_amount / 100; // Stripe stores in cents
   await Booking.create({ tour, user, price });
 };
 
